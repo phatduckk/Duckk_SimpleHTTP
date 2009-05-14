f1fc0ab46178874da641ddbea000c213f089f93c
Arin Sarkissian <Arin@arin.local>
2009-05-14 16:57:22 -0700

    comments and more info in the README

--- src/SimpleHttp/Exception.php
+++ src/SimpleHttp/Exception.php
@@ -1,5 +1,22 @@
 <?php
-
+/**
+ * Config
+ *
+ * PHP version 5.2.0+
+ *
+ * LICENSE: This source file is subject to the New BSD license that is
+ * available through the world-wide-web at the following URI:
+ * http://www.opensource.org/licenses/bsd-license.php. If you did not receive
+ * a copy of the New BSD License and are unable to obtain it through the web,
+ * please send a note to license@php.net so we can mail you a copy immediately.
+ *
+ * @category  SimpleHttp
+ * @package   SimpleHttp
+ * @author    Arin Sarkissian <arin@rspot.net>
+ * @copyright 2009 Arin Sarkissian
+ * @license   http://www.opensource.org/licenses/bsd-license.php New BSD License
+ * @version   CVS: $Id$
+ */
 class SimpleHttp_Exception extends Exception
 {
     
--- src/SimpleHttp/KeepAlive.php
+++ src/SimpleHttp/KeepAlive.php
@@ -1,9 +1,42 @@
 <?php
-
+/**
+ * Config
+ *
+ * PHP version 5.2.0+
+ *
+ * LICENSE: This source file is subject to the New BSD license that is
+ * available through the world-wide-web at the following URI:
+ * http://www.opensource.org/licenses/bsd-license.php. If you did not receive
+ * a copy of the New BSD License and are unable to obtain it through the web,
+ * please send a note to license@php.net so we can mail you a copy immediately.
+ *
+ * @category  SimpleHttp
+ * @package   SimpleHttp
+ * @author    Arin Sarkissian <arin@rspot.net>
+ * @copyright 2009 Arin Sarkissian
+ * @license   http://www.opensource.org/licenses/bsd-license.php New BSD License
+ * @version   CVS: $Id$
+ */
+ 
 require_once 'SimpleHttp.php';
 
+/**
+ * A simple class that decorates SimpleHttp to "force" Keep-Alive support.
+ * Essentially it just sets the Connection header and forces HTTP 1.1 
+ *
+ * @category  SimpleHttp
+ * @package   SimpleHttp
+ * @author    Arin Sarkissian <arin@rspot.net>
+ * @copyright 2009 Arin Sarkissian 
+ */
 class SimpleHttp_KeepAlive extends SimpleHttp
 {
+    /**
+     * Constructor
+     *
+     * Call the parent, set Connection header to Keep-Alive, and finally
+     * set the HTTP version to 1.1
+     */
     public function __construct($host, $port = 80)
     {
         parent::__construct($host, $port);
@@ -13,6 +46,12 @@ class SimpleHttp_KeepAlive extends SimpleHttp
         $this->setHttpVersion(self::HTTP_VERSION_1_1);
     }
     
+    /**
+     * Override parent::setHttpVersion() in order to not
+     * allow HTTP 1.0
+     *
+     * @param int $httpVersion Doesn't fuckin matter yo
+     */
     public function setHttpVersion($httpVersion)
     {
         // noop - don't the http version to change
